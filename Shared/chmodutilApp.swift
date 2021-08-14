db20489ce54dd77e9a629748e087033da416b3d7
Mattia Righetti <matt95.righetti@gmail.com>
2021-08-14 19:15:20 +0200

    Initial Commit

---
+++ Shared/chmodutilApp.swift
@@ -0,0 +1,17 @@
+//
+//  chmodutilApp.swift
+//  Shared
+//
+//  Created by Mattia Righetti on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct chmodutilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
